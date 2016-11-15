87ba880afd179ea99ac89a08adefed2a511b1a40
Arkadiusz Fijalkowski <fijalkowski.arek@gmail.com>
2016-11-15 11:29:23 +0100

    Finished module 7 - HTTP Get

--- program7.js
+++ program7.js
@@ -1,2 +1,12 @@
 var http = require('http');
 
+var url = process.argv[2];
+http.get(url, (res) => {
+  res.setEncoding('utf8');
+  res.on('data', (data) => {
+    console.log(data);
+  });
+  res.on('error', (err) => {
+    console.error(err);
+  });
+}).on('error', console.error);
